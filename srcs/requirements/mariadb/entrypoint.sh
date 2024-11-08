6477385ccf62a1c5ce2638d5608299175a701861
imisumi <ichiromisumi@outlook.com>
2024-11-08 01:32:59 +0100

    cleanup and added ssl for HTTPS

--- srcs/requirements/mariadb/entrypoint.sh
+++ srcs/requirements/mariadb/entrypoint.sh
@@ -1,67 +1,5 @@
 #!/bin/bash
 
-# echo "Starting MariaDB setup..."
-
-# # Start the MariaDB server in the background for setup
-# mysqld --skip-networking &
-# pid="$!"
-
-# echo "MariaDB is starting..."
-
-# # Wait for MariaDB to be ready
-# until mysqladmin ping >/dev/null 2>&1; do
-#   echo "Waiting for MariaDB to start..."
-#   sleep 2
-# done
-
-# echo "MariaDB is ready!"
-
-# # Run the SQL commands to initialize the database and users
-# cat <<EOF | mysql -u root
-# -- Set the root password (replace 'your_root_password' with a strong password)
-# ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
-
-# -- Create the WordPress database
-# CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
-
-# -- Create the normal user with a specified password
-# CREATE USER IF NOT EXISTS '${WORDPRESS_DB_USER}'@'%' IDENTIFIED BY '${WORDPRESS_DB_PASSWORD}';
-
-# -- Grant limited privileges to wpuser on the WordPress database
-# GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${WORDPRESS_DB_USER}'@'%';
-
-# -- Flush the privileges to ensure that they take effect
-# FLUSH PRIVILEGES;
-# EOF
-
-# # Stop the temporary MariaDB process
-# kill "$pid"
-# wait "$pid"
-
-# # Start MariaDB in the foreground (container's main process)
-# exec mysqld
-
-
-
-# cat <<EOF > /etc/mysql/init.sql
-# -- Set the root password
-# ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
-
-# -- Create the WordPress database
-# CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
-
-# -- Create the normal user with a specified password
-# CREATE USER IF NOT EXISTS '${WORDPRESS_DB_USER}'@'%' IDENTIFIED BY '${WORDPRESS_DB_PASSWORD}';
-
-# -- Grant limited privileges to wpuser on the WordPress database
-# GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${WORDPRESS_DB_USER}'@'%';
-
-# -- Flush the privileges to ensure that they take effect
-# FLUSH PRIVILEGES;
-# EOF
-
-# exec mysqld
-
 # Only create init.sql if it doesn't already exist
 if [ ! -f /etc/mysql/init.sql ]; then
   echo "Creating init.sql file..."
@@ -90,4 +28,7 @@ else
 fi
 
 # Start the MySQL server
-exec mysqld
+# exec mysqld
+
+# Execute the main command
+exec "$@"
--- srcs/requirements/wordpress/entrypoint.sh
+++ srcs/requirements/wordpress/entrypoint.sh
@@ -1,43 +1,5 @@
 #!/bin/bash
 
-# Wait for MariaDB to be ready
-# until mysqladmin --host=mariadb --user=wpuser --password=password --silent ping; do
-#     >&2 echo "mariadb is sleeping"
-#     sleep 10
-# done
-
-# WordPress Configuration
-# echo "wordpress env variables"
-# echo "WORDPRESS_DB_HOST: $WORDPRESS_DB_HOST"
-# echo "WORDPRESS_DB_USER: $WORDPRESS_DB_USER"
-# echo "WORDPRESS_DB_PASSWORD: $WORDPRESS_DB_PASSWORD"
-# echo "WORDPRESS_DB_NAME: $WORDPRESS_DB_NAME"
-# echo "WORDPRESS_SITE_URL: $WORDPRESS_SITE_URL"
-# echo "WORDPRESS_SITE_TITLE: $WORDPRESS_SITE_TITLE"
-# echo "WORDPRESS_ADMIN_USER: $WORDPRESS_ADMIN_USER"
-# echo "WORDPRESS_ADMIN_PASSWORD: $WORDPRESS_ADMIN_PASSWORD"
-# echo "WORDPRESS_ADMIN_EMAIL: $WORDPRESS_ADMIN_EMAIL"
-
-
-# # Download WordPress if not already downloaded
-# if [ ! -f wp-config.php ]; then
-#     wp core download --allow-root
-#     wp config create --dbname=wordpress --dbuser=wpuser --dbpass=password --dbhost=mariadb --allow-root
-#     wp core install --url=localhost --title=inception --admin_user=admin --admin_password=admin --allow-root
-# fi
-
-# WordPress Configuration
-# echo "wordpress env variables"
-# echo "WORDPRESS_DB_HOST: $WORDPRESS_DB_HOST"
-# echo "WORDPRESS_DB_USER: $WORDPRESS_DB_USER"
-# echo "WORDPRESS_DB_PASSWORD: $WORDPRESS_DB_PASSWORD"
-# echo "WORDPRESS_DB_NAME: $WORDPRESS_DB_NAME"
-# echo "WORDPRESS_SITE_URL: $WORDPRESS_SITE_URL"
-# echo "WORDPRESS_SITE_TITLE: $WORDPRESS_SITE_TITLE"
-# echo "WORDPRESS_ADMIN_USER: $WORDPRESS_ADMIN_USER"
-# echo "WORDPRESS_ADMIN_PASSWORD: $WORDPRESS_ADMIN_PASSWORD"
-# echo "WORDPRESS_ADMIN_EMAIL: $WORDPRESS_ADMIN_EMAIL"
-
 until mysqladmin --host=$WORDPRESS_DB_HOST --user=$WORDPRESS_DB_USER --password=$WORDPRESS_DB_PASSWORD --silent ping; do
 	>&2 echo "mariadb is sleeping"
 	sleep 10
